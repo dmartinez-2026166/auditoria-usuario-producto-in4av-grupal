cb84ae58dc6e1735ab8d6684bdfc2faff411e293
Diego Martinez <dmartinez-2026166@kinal.edu.gt>
2026-08-26 14:15:46 -0600

    feat: se actualizo la clase Validations.java

--- src/com/diegomartinez/system/utils/Validations.java
+++ src/com/diegomartinez/system/utils/Validations.java
@@ -5,16 +5,42 @@ public class Validations {
     public Validations() {
 
     }
-    
+
     public Boolean validateTextEmpty(String text) {
         boolean isEmpty = false;
-        if(text.isEmpty() == true || text.isBlank() == true) {
+        if (text.isEmpty() == true || text.isBlank() == true) {
             isEmpty = true;
         }
         return isEmpty;
     }
-    
+
     public Boolean validateTextLength(String text, int textMax) {
-        return text.length()<=textMax;
+        return text.length() <= textMax;
+    }
+
+    public Boolean equalsText(String textOriginal, String textCompare) {
+        return textOriginal.equals(textCompare);
+    }
+
+    public Boolean validateEmail(String email) {
+        int dotCount = 0, arrobeCount = 0;
+        for (int index = 0; index < email.length(); index++) {
+            if (email.charAt(index) == '.') {
+                dotCount++;
+                if (dotCount > 1) {
+                    return false;
+                }
+            }
+        }
+
+        for (int index = 0; index < email.length(); index++) {
+            if (email.charAt(index) == '@') {
+                arrobeCount++;
+                if (arrobeCount != 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
